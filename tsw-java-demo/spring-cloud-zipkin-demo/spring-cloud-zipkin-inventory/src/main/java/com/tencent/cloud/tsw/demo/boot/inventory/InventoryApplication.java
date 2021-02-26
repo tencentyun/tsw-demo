1f3ef50fa5b93448bf33b827316b08db588de69d
hongweizhu <hongweizhu@tencent.com>
2021-02-26 11:06:38 +0800

    modify restTemplate timeout modify consul ttl configuration

--- tsw-java-demo/spring-cloud-zipkin-demo/spring-cloud-zipkin-inventory/src/main/java/com/tencent/cloud/tsw/demo/boot/inventory/InventoryApplication.java
+++ tsw-java-demo/spring-cloud-zipkin-demo/spring-cloud-zipkin-inventory/src/main/java/com/tencent/cloud/tsw/demo/boot/inventory/InventoryApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,7 +25,10 @@ public class InventoryApplication {
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(3000);
+        requestFactory.setReadTimeout(20000);
+        return new RestTemplate(requestFactory);
     }
 
 }
